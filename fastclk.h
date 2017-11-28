7e617b3231d6bcf325338830cddb5f73566b5786
Mublue <bluecard82@gmail.com>
2017-11-28 16:17:10 +0300

    Update fastclk.h

--- fastclk.h
+++ fastclk.h
@@ -27,7 +27,7 @@ unsigned char wclk_high[2][2] =
 {0x96,0x9E}
 };
 
-unsigned int imsk[32] =
+unsigned int imask[32] =
  {0x00000001, 0x00000002, 0x00000004, 0x00000008, 
  0x00000010, 0x00000020, 0x00000040, 0x00000080, 
  0x00000100, 0x00000200, 0x00000400, 0x00000800,
@@ -36,9 +36,6 @@ unsigned int imsk[32] =
  0x00100000, 0x00200000, 0x00400000, 0x00800000, 
 0x01000000, 0x02000000, 0x04000000, 0x08000000
 0x10000000, 0x20000000, 0x40000000,0x80000000};
-unsigned int clockin_fast (unsigned char lowclk,unsigned char highclk,unsigned char imask);
-// new way to send direct values to Lpt
-// without heavy math instruction could
-// cost big time with megabytes flashed
+
 
 #endif
